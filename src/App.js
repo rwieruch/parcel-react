e7ed95bd8686740e112b8696f12d8adc82c1ff4e
Robin Wieruch <wrobin@gmx.net>
2018-02-11 20:21:57 +0100

    update to recent version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,8 +19,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro" onClick={() => this.setState({ counter: 2 })}>
+          To get started, edit <code>src/App.js</code> and save to reload. {this.state.counter}
         </p>
       </div>
     );
