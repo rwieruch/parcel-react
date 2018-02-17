dd388a0f9ffbe71da6a0c3e3e11cfad5a066ada0
Robin Wieruch <wrobin@gmx.net>
2018-02-17 16:23:45 +0100

    remove unused state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      counter: 0,
-    }
-  }
-
   render() {
     return (
       <div className="App">
@@ -19,8 +11,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro" onClick={() => this.setState({ counter: 2 })}>
-          To get started, edit <code>src/App.js</code> and save to reload. {this.state.counter}
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
